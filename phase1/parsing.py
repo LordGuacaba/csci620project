6c2c5534ebe6d46b3f112ad4255805e81e9455ae
Scott LoPinto <scott@Scotts-MacBook-Air.local>
2025-10-16 14:16:08 -0400

    tabbed out some more parsing functions

--- phase1/parsing.py
+++ phase1/parsing.py
@@ -21,9 +21,9 @@ TABLE_MAP = {
     # in which case we can map that to a different map and update this to a more
     # standalone entity map (i.e no FKs)
 
-    #"AtBats":{"columns":["number", "game", "batter", "inning", "top", "pitches", "play", "playDetails", "baserunnerDetails"], "file": ""},
-    #"Games":{"columns":["id", "homeTeam", "visTeam", "date", "location", "useDH", "htbf", "attendance", "wp", "lp", "save"], "file": ""},
-    #"PlayerActivity":{"columns":["gameId", "playerId", "team", "battingPosition", "fieldingPosition", "inning", "pinchHit", "pinchRun"], "file": ""},
+    # "AtBats":{"columns":["number", "game", "batter", "inning", "top", "pitches", "play", "playDetails", "baserunnerDetails"], "file": ""},
+    # "Games":{"columns":["id", "homeTeam", "visTeam", "date", "location", "useDH", "htbf", "attendance", "wp", "lp", "save"], "file": ""},
+    # "PlayerActivity":{"columns":["gameId", "playerId", "team", "battingPosition", "fieldingPosition", "inning", "pinchHit", "pinchRun"], "file": ""},
 
 }
 
@@ -73,6 +73,137 @@ def connect_to_db():
     return psycopg2.connect(**DB)
 
 
+def parse_info_line(line):
+    """
+    this is extremely variable, so we will just return the line for now
+    """
+    pass
+
+def parse_id_line(line):
+    """
+    first 3 letters id home team
+    next 4 are the year
+    next 2 are month
+    next 2 are day
+    last number is single game(0), first game(1), or second game(2)
+    """
+    pass
+def parse_start_and_sub_line(line):
+    """
+    1. The first field is the Retrosheet player id, which is unique for each player.
+    
+    2. The second field is the player's name.
+
+    3. The next field is either 0 (for visiting team), or 1 (for home team). 
+        In some games, typically due to scheduling conflicts, the home team (the team whose stadium the game is played in) 
+        bats first (in the top of the innings) and the visiting team bats second (in the bottom of the innings). 
+        In these games, contrary to "normal" games, start records for the home team ("1") precede start records for the visiting team ("0"). 
+        Similarly, the play codes pertaining to the home team ("1") precede the play codes pertaining to the visiting team ("0").
+
+    4. The next field is the position in the batting order, 1 - 9. 
+        When a game is played using the DH rule the pitcher is given the batting order position 0.
+
+    5. The last field is the fielding position. 
+        The numbers are in the standard notation, with designated hitters being identified as position 10. 
+        On sub records 11 indicates a pinch hitter and 12 is used for a pinch runner. 
+        When a player pinch hits or pinch runs for the DH, that player automatically becomes the DH, 
+        so no 'sub' record is included to identify the new DH.
+    """
+    pass
+
+def parse_play_line(line):
+    """
+    play The play records contain the events of the game. Each play record has 6 fields after "play".
+
+    1. The first field is the inning, an integer starting at 1.
+
+    2. The second field is either 0 (for visiting team) or 1 (for home team).
+
+    3. The third field is the Retrosheet player id of the player at the plate.
+
+    4. The fourth field is the count on the batter when this particular event (play) occurred. 
+        Most games prior to 1988 do not have this information, and in such cases, "??" appears in this field.
+
+    5. The fifth field is of variable length and contains all pitches to this batter in this plate appearance and is described below. 
+        If pitches are unknown, this field is left empty with nothing in between the commas.
+
+    6. The sixth field describes the play or event that occurred.
+    """
+    pass
+def parse_com_line(line):
+    """
+    The final record type is used primarily to add explanatory information for a play. 
+    Although it may occur anywhere in a file, it is usually not present until after the start records. 
+    The second field of the com record is quoted.
+
+        com,"ML debut for Behenna"
+    """
+    pass
+def parse_data_line(line):
+    """
+    Data records appear after all records from the game. 
+    At present, the only data type, that is defined specifies the number of earned runs allowed by a pitcher. 
+    Each such record contains the pitcher's Retrosheet player id and the number of earned runs he allowed. 
+    There is a data record for each pitcher that appeared in the game.
+
+        data,er,showe001,2
+    """
+    pass
+
+def parse_badj_line(line):
+    """
+    batter adjustment
+    This record is used to mark a plate appearance in which the batter bats from the side that is not expected. The syntax is:
+        badj,batter id,hand
+    therefore as an example:
+
+        badj,bonib001,R
+    """
+    pass
+def parse_padj_line(line):
+    """
+    pitcher adjustment
+        padj,pitcher id,hand
+    The expectation is defined by the roster file and an example is:
+
+        padj,harrg001,L
+    """
+    pass
+def parse_ladj_line(line):
+    """
+    "Lineup adjustment". 
+    This record is used when a team bats out of order. The syntax is:
+
+        ladj,batting team,batting order position
+
+    therefore as an example:
+
+        ladj,0,4
+    """
+    pass
+def parse_radj_line(line):
+    """
+    "Runner adjustment". 
+    This record is used in games beginning in 2020 in which an extra inning begins with a runner on 2nd. The syntax is:
+
+        radj,runner id,base
+
+    therefore as an example:
+
+        radj,turnj001,2
+    """
+    pass
+def parse_presadj_line(line):
+    """
+    "Pitcher responsibility adjustment"
+        presadj,pitcher id,base occupied by relevant runner
+
+    therefore as an example:
+
+        presadj,cicoe001,2
+
+    """
+    pass
 
 #### -------- #### ---------------------- example usage below ----------------- #### -------- ####
 def main():
