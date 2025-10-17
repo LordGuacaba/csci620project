c198cb7504d9c05707b73ef1abb73f28f10f2602
Scott LoPinto <scott@Scotts-MacBook-Air.local>
2025-10-17 11:33:48 -0400

    recursive function, some event files to test it

---
+++ phase1/recursive_directory_read.py
@@ -0,0 +1,31 @@
+def recursiverly_read_directory(path, file_list=None):
+    """
+    Recursively reads a directory and returns a list of all file paths within it
+    """
+    import os
+
+    DIR = "../data/eventFiles"
+
+    if file_list is None:
+        file_list = []
+
+    for entry in os.scandir(path):
+        if entry.is_file():
+            file_list.append(entry.path)
+        elif entry.is_dir():
+            recursiverly_read_directory(entry.path, file_list)
+
+    return file_list
+
+
+def main():
+    import os
+
+    DIR = "../data/eventFiles"
+    all_files = recursiverly_read_directory(DIR)
+    for file_path in all_files:
+        print(file_path)
+
+
+if __name__ == "__main__":
+    main()
